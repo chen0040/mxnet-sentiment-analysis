9e1f6c36edef2695c9e26b6b000aa5181ac1fb50
xschen <xs0040@gmail.com>
2018-05-12 10:58:55 +0800

    demo codes

---
+++ demo/lstm_predict.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+import mxnet as mx
+import os
+import sys
+
+from sklearn.model_selection import train_test_split
+
+
+def patch_path(path):
+    return os.path.join(os.path.dirname(__file__), path)
+
+
+def main():
+    sys.path.append(patch_path('..'))
+
+    mx.random.seed(1)
+
+    model_dir_path = os.path.join(os.path.dirname(__file__), 'models')
+    data_file_path = patch_path('data/umich-sentiment-train.txt')
+
+    from mxnet_nlp.library.sentiment_analysis.lstm import SentimentAnalyserWithSoftMaxLSTM
+    from mxnet_nlp.library.utility.simple_data_loader import load_text_label_pairs
+
+    text_label_pairs = load_text_label_pairs(data_file_path)
+
+    train_data, validation_data = train_test_split(text_label_pairs, test_size=0.3, random_state=42)
+
+    rnn = SentimentAnalyserWithSoftMaxLSTM(model_ctx=mx.gpu(0), data_ctx=mx.gpu(0))
+    rnn.load_model(model_dir_path)
+
+    for text, label in validation_data:
+        predicted_label = rnn.predict_class(text)
+        print('predicted: ', predicted_label, 'actual: ', label)
+
+
+if __name__ == '__main__':
+    main()
---
+++ demo/lstm_train.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+import mxnet as mx
+import os
+import sys
+
+from sklearn.model_selection import train_test_split
+
+
+def patch_path(path):
+    return os.path.join(os.path.dirname(__file__), path)
+
+
+def main():
+    sys.path.append(patch_path('..'))
+
+    mx.random.seed(1)
+
+    output_dir_path = os.path.join(os.path.dirname(__file__), 'models')
+    data_file_path = patch_path('data/umich-sentiment-train.txt')
+
+    from mxnet_nlp.library.sentiment_analysis.lstm import SentimentAnalyserWithSoftMaxLSTM
+    from mxnet_nlp.library.utility.simple_data_loader import load_text_label_pairs
+    from mxnet_nlp.library.utility.text_utils import fit_text
+
+    text_data_model = fit_text(data_file_path)
+    text_label_pairs = load_text_label_pairs(data_file_path)
+
+    train_data, validation_data = train_test_split(text_label_pairs, test_size=0.3, random_state=42)
+
+    rnn = SentimentAnalyserWithSoftMaxLSTM(model_ctx=mx.gpu(0), data_ctx=mx.gpu(0))
+    history = rnn.fit(text_data_model, text_label_pairs=train_data, model_dir_path=output_dir_path,
+                      checkpoint_interval=10, batch_size=64, epochs=20, test_text_label_pairs=validation_data)
+
+
+if __name__ == '__main__':
+    main()
